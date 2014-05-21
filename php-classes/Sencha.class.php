82b767c60efe8f6950764dd0d0d864254961a711
Chris Alfano <chris@jarv.us>
2014-05-21 19:20:53 -0400

    bump default Sencha CMD version

--- php-classes/Sencha.class.php
+++ php-classes/Sencha.class.php
@@ -18,7 +18,7 @@ class Sencha
 		)
 	);
 	
-	static public $defaultCmdVersion = '4.0.2.67';
+	static public $defaultCmdVersion = '4.0.4.84';
 	
 	static public $cmdPath = '/usr/local/bin/Sencha/Cmd';
 	static public $binPaths = array('/bin','/usr/bin','/usr/local/bin');
