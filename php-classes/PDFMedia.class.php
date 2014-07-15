affb1888bbf39aa35f6f6935f398ca91519c0653
Chris Alfano <chris@jarv.us>
2014-07-14 22:50:14 -0400

    media classes expanded to handle ai, eps, psd

--- php-classes/PDFMedia.class.php
+++ php-classes/PDFMedia.class.php
@@ -3,45 +3,48 @@
 class PDFMedia extends Media
 {
 
-	// configurables
+    // configurables
 	public static $extractPageCommand = 'convert \'%1$s[%2$u]\' JPEG:- 2>/dev/null'; // 1=pdf path, 2=page
 	public static $extractPageIndex = 0;
-	
-	
+
+
 	// magic methods
 	static public function __classLoaded()
 	{
 		$className = get_called_class();
-		
+
 		Media::$mimeHandlers['application/pdf'] = $className;
-		
+        Media::$mimeHandlers['application/postscript'] = $className;
+
 		parent::__classLoaded();
 	}
-	
-	
+
+
 	function getValue($name)
 	{
 		switch($name)
 		{
 			case 'ThumbnailMIMEType':
-				return 'image/jpeg';
-				
+				return 'image/png';
+
 			case 'Extension':
 
 				switch($this->MIMEType)
 				{
 					case 'application/pdf':
 						return 'pdf';
+					case 'application/postscript':
+						return 'eps';
 					default:
 						throw new Exception('Unable to find document extension for mime-type: ' . $this->MIMEType);
 				}
-				
+
 			default:
 				return parent::getValue($name);
 		}
 	}
-	
-	
+
+
 	// public methods
 	public function getImage($sourceFile = null)
 	{
@@ -51,24 +54,20 @@ class PDFMedia extends Media
 		}
 
 		$cmd = sprintf(static::$extractPageCommand, $sourceFile, static::$extractPageIndex);
-		
-		$fileData = shell_exec($cmd);
-		$fileImage = imagecreatefromstring($fileData);
-		
+		$fileImage = imagecreatefromstring(shell_exec($cmd));
+
 		return $fileImage;
 	}
-	
+
 	// static methods
 	static public function analyzeFile($filename, $mediaInfo = array())
 	{
 		$cmd = sprintf(static::$extractPageCommand, $filename, static::$extractPageIndex);
 		$pageIm = imagecreatefromstring(shell_exec($cmd));
-		
+
 		$mediaInfo['width'] = imagesx($pageIm);
 		$mediaInfo['height'] = imagesy($pageIm);
-	
+
 		return $mediaInfo;
 	}
-	
-
 }
