318557add9f64f7b5af5199ece3e3ac5fa5e1b9f
Chris Alfano <chris@jarv.us>
2014-08-20 12:55:54 -0400

    added summary field for blog posts

---
+++ sencha-workspace/packages/emergence-cms-summaries/src/field/Summary.js
@@ -0,0 +1,8 @@
+/*jslint browser: true, undef: true *//*global Ext*/
+Ext.define('Emergence.cms.summaries.field.Summary', {
+    extend: 'Ext.form.field.TextArea',
+    xtype: 'emergence-cms-summaryfield',
+
+    emptyText: 'Post summary',
+    grow: true
+});
