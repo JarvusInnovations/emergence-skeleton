403b005e7e56710caf08443ae8e7aa513b16bc3f
Chris Alfano <chris@jarv.us>
2014-09-09 18:06:26 -0400

    added capitalizer and renamed handle option

--- php-classes/Emergence/Util/Capitalizer.php
+++ php-classes/Emergence/Util/Capitalizer.php
@@ -19,7 +19,7 @@ class Capitalizer
         $_recurse = function($word) use ($me, $familyName) {
             return $me::capitalizePronoun($word, $familyName);
         };
-        
+
         if (preg_match('/^[ea]l-\pL/u', $word)) {
             // el- / al- prefixes stay lowercase
             return strtolower(substr($word, 0, 2)) . '-' . static::capitalizePronoun(substr($word, 3));
@@ -27,27 +27,27 @@ class Capitalizer
             // process hyphenated-separated bits independently
             return implode('-', array_map($_recurse, explode('-', $word)));
         }
-        
+
         if (strpos($word, ' ') !== false) {
             // process space-separated bits independently
             return implode(' ', array_map($_recurse, explode(' ', $word)));
         }
-        
+
         if (strpos($word, '\'') !== false) {
             // process apostrophe-separated bits independently
             return implode('\'', array_map($_recurse, explode('\'', $word)));
         }
-        
+
         if (strpos($word, '.') !== false) {
             // process .-separated bits independently
             return implode('.', array_map($_recurse, explode('.', $word)));
         }
-        
+
         // roman numerals (only detects 1-14) should be all caps
         if (preg_match('/^(i{1,3}|i?vi{0,3}|i?xi{0,3})$/i', $word)) {
             return strtoupper($word);
         }
-        
+
         // start out all-lowercase
         $word = strtolower($word);
 
@@ -59,11 +59,11 @@ class Capitalizer
             foreach (static::$familyNamePrefixes AS $prefix) {
                 if (strpos($word, $prefix) === 0) {
                     $prefixLen = strlen($prefix);
-                    
+
                     if (
                         // skip if a double letter follows the prefix (e.g. Derry)
                         !preg_match('/^([a-z])\1/', substr($word, $prefixLen)) &&
-                        
+
                         // skip if only one letter is left after the prefix
                         strlen($word) > $prefixLen + 1
                     ) {
--- php-classes/HandleBehavior.class.php
+++ php-classes/HandleBehavior.class.php
@@ -3,7 +3,7 @@
 class HandleBehavior extends RecordBehavior
 {
     public static $alwaysSuffix = false;
-    public static $format = '%s-%u';
+    public static $incrementerFormat = '%s-%u';
     public static $transliterate = true;
 
     public static function onSave(ActiveRecord $Record, $handleInput = false)
@@ -40,7 +40,7 @@ class HandleBehavior extends RecordBehavior
             'handleField' => 'Handle'
             ,'domainConstraints' => array()
             ,'alwaysSuffix' => static::$alwaysSuffix
-            ,'format' => static::$format
+            ,'incrementerFormat' => static::$incrementerFormat
             ,'transliterate' => static::$transliterate
             ,'case' => 'lower' // 'lower' / 'upper' / null
         ), $options);
@@ -92,7 +92,7 @@ class HandleBehavior extends RecordBehavior
             $incarnation++;
 
             if ($options['alwaysSuffix'] || $incarnation > 1) {
-                $handle = sprintf($options['format'], $text, $incarnation);
+                $handle = sprintf($options['incrementerFormat'], $text, $incarnation);
             }
             
             $where[$options['handleField']] = $handle;
