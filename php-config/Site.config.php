34324172b181884f876ed2b95a2d97260fd892e9
Henry Paradiz <henry.paradiz@gmail.com>
2014-09-22 14:11:15 -0400

    Added an example method to Site.config.php for doing custom routing.

--- php-config/Site.config.php
+++ php-config/Site.config.php
@@ -14,3 +14,20 @@ Site::$skipSessionPaths[] = 'thumbnail.php';
 
 // uncomment or set to an array of specific hostnames to enable CORS
 //Site::$permittedOrigins = '*';
+
+// Custom routing called if a page isn't found in site-root
+/*Site::$onNotFound = function($message) {
+	switch($action = Site::$requestPath[0])
+	{
+		default:
+			if($Page = Page::getByHandle($action))
+			{
+				return Page::renderPage();
+			}
+			else
+			{
+				header('HTTP/1.0 404 Not Found');
+				die($message);
+			}
+	}
+};*/
