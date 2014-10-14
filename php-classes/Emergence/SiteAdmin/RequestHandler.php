49892cd43d4445512461c10cd8c560135f880e2f
Chris Alfano <chris@jarv.us>
2014-10-14 14:58:43 -0400

    call cacheTree for site-admin tools before listing

--- php-classes/Emergence/SiteAdmin/RequestHandler.php
+++ php-classes/Emergence/SiteAdmin/RequestHandler.php
@@ -7,7 +7,9 @@ class RequestHandler extends \RequestHandler
     public static function handleRequest()
     {
         $GLOBALS['Session']->requireAccountLevel('Developer');
-        
+
+        \Emergence_FS::cacheTree('site-root/site-admin');
+
         return static::respond('siteAdminIndex', array(
             'scripts' => array_filter(
                 \Emergence_FS::getAggregateChildren('site-root/site-admin'),
