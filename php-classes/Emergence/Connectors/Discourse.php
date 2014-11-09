903d59be66593acbddfe1630cd2a3ff0443a36df
Chris Alfano <chris@jarv.us>
2014-11-09 01:06:38 -0500

    pass about and avatar to discourse

--- php-classes/Emergence/Connectors/Discourse.php
+++ php-classes/Emergence/Connectors/Discourse.php
@@ -2,6 +2,7 @@
 
 namespace Emergence\Connectors;
 
+use Site;
 use Emergence\People\IPerson;
 
 class Discourse extends \Emergence\Connectors\AbstractConnector implements \Emergence\Connectors\IIdentityConsumer
@@ -24,29 +25,38 @@ class Discourse extends \Emergence\Connectors\AbstractConnector implements \Emer
             return static::throwInvalidRequestError('sig parameter missing');
         }
         
+        if ($_GET['sig'] != hash_hmac('sha256', $_GET['sso'], static::$ssoSecret)) {
+            return static::throwInvalidRequestError('sig is invalid');
+        }
+        
         if (!static::$host || !static::$ssoSecret) {
             return static::throwError('Discourse SSO is not fully configured yet');
         }
 
-        if ($_GET['sig'] != hash_hmac('sha256', $_GET['sso'], static::$ssoSecret)) {
-            return static::throwInvalidRequestError('sig is invalid');
-        }
 
         // decode payload into associative array
         $payload = base64_decode($_GET['sso']);
         parse_str($payload, $payload);
+
 
         // append return values to payload
         $payload['name'] = $Person->FullName;
         $payload['email'] = $Person->Email;
         $payload['username'] = $Person->Username;
         $payload['external_id'] = $Person->ID;
+        $payload['about_me'] = $Person->About;
+        
+        if ($Person->PrimaryPhoto) {
+            $payload['avatar_url'] = 'http://' . Site::getConfig('primary_hostname') . $Person->PrimaryPhoto->WebPath;
+        }
+
 
         // re-encode payload
         $payload = base64_encode(http_build_query($payload));
 
+
         // redirect with payload and its signature
-        \Site::redirect('http://' . static::$host . '/session/sso_login', [
+        Site::redirect('http://' . static::$host . '/session/sso_login', [
             'sso' => $payload,
             'sig' => hash_hmac('sha256', $payload, static::$ssoSecret)
         ]);
