a8cd27d6b9400d59ba466d41cf8edf2cb3193fe1
Chris Alfano <chris@jarv.us>
2015-11-01 01:04:25 -0400

    Removed old unused classes

--- php-classes/BlogAdminRequestHandler.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-class BlogAdminRequestHandler extends RequestHandler {
-	static public function handleRequest() {
-		$GLOBALS['Session']->requireAccountLevel('Staff');
-		
-		static::respond('blog/admin/home');
-	}
-}
