5b3b4cbf3604542a931c6448fa2f7ca95209a2a9
Chris Alfano <chris@jarv.us>
2015-12-14 14:02:41 -0500

    Add support for embed option to cssmin template plugin and normalize formatting

--- dwoo-plugins/jsmin.php
+++ dwoo-plugins/jsmin.php
@@ -5,24 +5,22 @@ function Dwoo_Plugin_jsmin(Dwoo_Core $dwoo, $files, $root = array('site-root','j
 	if (is_array($files)) {
 		$files = implode('+', $files);
 	}
-	
+
 	// analyze tree to obtain hash and file map
 	$sourceReport = MinifiedRequestHandler::getSourceReport($files, $root, 'application/javascript');
-	
+
 	if(!empty($_GET['js-debug']) || !empty($_GET['jsdebug'])) {
 		$html = '';
 		foreach($sourceReport['files'] AS $filename => $fileData) {
 			$html .= "<script src='".preg_replace('/^site-root/', '', $filename)."?_sha1=$fileData[SHA1]'></script>\n";
 		}
-		
+
 		return $html;
 	}
-	
-	if($embed) {
+
+	if ($embed) {
 		return '<script>'.MinifiedRequestHandler::getSourceCode('JSMin', $sourceReport).'</script>';
-	}
-	else {
+	} else {
 		return "<script src='/min/js/$files?_sha1=$sourceReport[hash]'></script>";
 	}
 }
-
