c1ad52a3e3e01141e98d1715b132af4ff14362ee
Chris Alfano <chris@jarv.us>
2015-12-14 14:02:59 -0500

    Add includeHost option to versioned_url template plugin

--- dwoo-plugins/versioned_url.php
+++ dwoo-plugins/versioned_url.php
@@ -1,13 +1,18 @@
 <?php
 
-function Dwoo_Plugin_versioned_url(Dwoo_Core $dwoo, $path, $source = 'site-root')
+function Dwoo_Plugin_versioned_url(Dwoo_Core $dwoo, $path, $source = 'site-root', $includeHost = false)
 {
     $trimmedPath = ltrim($path, '/');
-    
+
     if ($source == 'site-root') {
-        return Site::getVersionedRootUrl($trimmedPath);
+        $url = Site::getVersionedRootUrl($trimmedPath);
+
+        if ($includeHost) {
+            $url = (Site::getConfig('ssl') ? 'https' : 'http') . '://' . Site::getConfig('primary_hostname') . $url;
+        }
+
+        return $url;
     } else {
         return $path;
     }
 }
-
