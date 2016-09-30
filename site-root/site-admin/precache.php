c3e822c852de2a4b6c46088eaa53a2fff9755e5b
Chris Alfano <chris@jarv.us>
2016-09-29 21:27:14 -0400

    Remove old trees array and skip empty collection paths

--- site-root/site-admin/precache.php
+++ site-root/site-admin/precache.php
@@ -8,28 +8,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     set_time_limit(0);
 
-    $trees = array(
-        'dwoo-plugins'
-        ,'event-handlers'
-        ,'ext-library'
-        ,'html-templates'
-        ,'js-library'
-        ,'php-classes'
-        ,'php-config'
-        ,'phpunit-tests'
-        ,'php-migrations'
-        ,'site-root'
-        ,'sencha-workspace/.sencha'
-        ,'sencha-workspace/microloaders'
-        ,'sencha-workspace/pages'
-        ,'sencha-workspace/packages'
-        ,'sencha-workspace/EmergenceEditor'
-        ,'sencha-workspace/EmergencePullTool'
-        ,'sencha-workspace/ContentEditor'
-    );
-
     $message = "";
     foreach ($_POST['collections'] AS $collection) {
+        if (!$collection = trim($collection)) {
+            continue;
+        }
+
         $filesCached = Emergence_FS::cacheTree($collection, true);
         $message .= sprintf('Precached %03u files in %s'.PHP_EOL, $filesCached, $collection);
     }
