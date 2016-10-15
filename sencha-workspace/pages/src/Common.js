8af16ec3122c9ace44f1b777529e392296257f1b
Chris Alfano <chris@jarv.us>
2016-10-15 15:55:34 -0400

    Add skeleton-v2 pages JS overrides to source control

---
+++ sencha-workspace/pages/src/Common.js
@@ -0,0 +1,25 @@
+/*jslint browser: true, undef: true *//*global Ext*/
+Ext.define('Site.Common', {
+    singleton: true,
+    requires: [
+        //'Site.widget.ContentBlocks',
+        'Site.widget.Search',
+        'Site.widget.model.Person',
+        'Site.widget.model.Tag',
+        'Site.widget.model.Content'
+    ],
+
+    constructor: function() {
+        Ext.onReady(this.onDocReady, this);
+    },
+
+    onDocReady: function() {
+        var me = this,
+            body = Ext.getBody();
+
+        // site search
+        me.siteSearch = Ext.create('Site.widget.Search', {
+            searchForm: body.down('.search-form.site-search')
+        });
+    }
+});
