d4f5e90db51427c554480c2a44af26866add1d3e
Chris Alfano <chris@jarv.us>
2016-12-02 14:50:21 -0500

    Add disk and memory stats to dashboard

--- php-classes/Emergence/SiteAdmin/DashboardRequestHandler.php
+++ php-classes/Emergence/SiteAdmin/DashboardRequestHandler.php
@@ -2,8 +2,10 @@
 
 namespace Emergence\SiteAdmin;
 
+use Site;
 use Person;
 use User;
+use Emergence\Util\ByteSize;
 
 
 class DashboardRequestHandler extends \RequestHandler
@@ -12,6 +14,25 @@ class DashboardRequestHandler extends \RequestHandler
     {
         $GLOBALS['Session']->requireAccountLevel('Administrator');
 
+        // get available memory
+        $availableMemory = null;
+        $availableSwap = null;
+
+        $memoryOutput = explode(PHP_EOL, trim(shell_exec('free -b')));
+        array_shift($memoryOutput);
+
+        foreach ($memoryOutput AS $line) {
+            $line = preg_split('/\s+/', $line);
+
+            if ($line[0] == 'Mem:') {
+                $availableMemory = $line[3];
+            } elseif ($line[0] == 'Swap:') {
+                $availableSwap = $line[3];
+            }
+        }
+
+
+        // render
         return static::respond('dashboard', [
             'metrics' => [
                 [
@@ -33,6 +54,18 @@ class DashboardRequestHandler extends \RequestHandler
                     'label' => 'Developers',
                     'value' => User::getCount(['AccountLevel' => 'Developer']),
                     'link' => '/people?q=accountlevel:Developer'
+                ],
+                [
+                    'label' => 'Available Storage',
+                    'value' => ByteSize::format(exec('df --output=avail ' . escapeshellarg(Site::$rootPath)))
+                ],
+                [
+                    'label' => 'Available Memory',
+                    'value' => $availableMemory ? ByteSize::format($availableMemory) : null
+                ],
+                [
+                    'label' => 'Available Swap',
+                    'value' => $availableSwap ? ByteSize::format($availableSwap) : null
                 ]
             ]
         ]);
