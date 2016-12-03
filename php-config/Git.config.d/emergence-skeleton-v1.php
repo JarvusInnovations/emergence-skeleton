28387e5d98d303a5f19946eb495f02a9371d0074
Chris Alfano <chris@jarv.us>
2016-12-03 15:28:36 -0500

    Restore conditional for skeleton repo config

--- php-config/Git.config.d/emergence-skeleton-v1.php
+++ php-config/Git.config.d/emergence-skeleton-v1.php
@@ -1,22 +1,24 @@
 <?php
 
-Git::$repositories['emergence-skeleton-v1'] = [
-    'remote' => 'https://github.com/JarvusInnovations/emergence-skeleton.git',
-    'originBranch' => 'master',
-    'workingBranch' => 'master',
-    'trees' => [
-        'dwoo-plugins',
-        'event-handlers',
-        'ext-library',
-        'html-templates',
-        'js-library',
-        'php-classes',
-        'php-config',
-        'php-migrations',
-        'phpunit-tests',
-        'sencha-build',
-        'sencha-workspace',
-        'site-root',
-        'site-tasks'
-    ]
-];
+if (Site::getConfig('handle') == 'skeleton-v1') {
+    Git::$repositories['emergence-skeleton-v1'] = [
+        'remote' => 'https://github.com/JarvusInnovations/emergence-skeleton.git',
+        'originBranch' => 'master',
+        'workingBranch' => 'master',
+        'trees' => [
+            'dwoo-plugins',
+            'event-handlers',
+            'ext-library',
+            'html-templates',
+            'js-library',
+            'php-classes',
+            'php-config',
+            'php-migrations',
+            'phpunit-tests',
+            'sencha-build',
+            'sencha-workspace',
+            'site-root',
+            'site-tasks'
+        ]
+    ];
+}
