ac531f97af51309381d2e883b2bff5c22107ee88
Nafis Bey <nafis@jarv.us>
2017-02-23 15:10:30 -0500

    Remove unneeded code and cast values to string

--- php-classes/Emergence/Connectors/Exceptions/SyncException.php
+++ php-classes/Emergence/Connectors/Exceptions/SyncException.php
@@ -29,18 +29,13 @@ class SyncException extends \Exception
         return $this->context;
     }
 
-    protected static function interpolate($message, array $context = array())
+    public static function interpolate($message, array $context = [])
     {
-        // build a replacement array with braces around the context keys
-        $replace = array();
-        foreach ($context as $key => $val) {
-            // check that the value can be casted to string
-            if (!is_array($val) && (!is_object($val) || method_exists($val, '__toString'))) {
-                $replace['{' . $key . '}'] = $val;
-            }
+        $replace = [];
+        foreach ($context as $key => $value) {
+            $replace['{' . $key . '}'] = (string)$value;
         }
 
-        // interpolate replacement values into the message and return
         return strtr($message, $replace);
     }
 }
--- php-classes/Emergence/Connectors/SyncResult.php
+++ php-classes/Emergence/Connectors/SyncResult.php
@@ -33,7 +33,6 @@ class SyncResult
 
     public function getMessage()
     {
-#        return $this->message;
         return static::interpolate($this->message, $this->context);
     }
 
@@ -46,18 +45,13 @@ class SyncResult
         }
     }
 
-    public static function interpolate($message, array $context = array())
+    public static function interpolate($message, array $context = [])
     {
-        // build a replacement array with braces around the context keys
-        $replace = array();
-        foreach ($context as $key => $val) {
-            // check that the value can be casted to string
-            if (!is_array($val) && (!is_object($val) || method_exists($val, '__toString'))) {
-                $replace['{' . $key . '}'] = $val;
-            }
+        $replace = [];
+        foreach ($context as $key => $value) {
+            $replace['{' . $key . '}'] = (string)$value;
         }
 
-        // interpolate replacement values into the message and return
         return strtr($message, $replace);
     }
 
