703599f58aa1930da7b32529278c7c0d979af170
Chris Alfano <chris@jarv.us>
2017-08-18 20:17:03 -0400

    Add Util::buildAbsolute

---
+++ php-classes/Emergence/Util/Url.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Emergence\Util;
+
+use Site;
+
+
+class Url
+{
+    public static function buildAbsolute($path = null, $params = null)
+    {
+        $url = Site::getConfig('ssl') ? 'https' : 'http';
+        $url .= '://' . Site::getConfig('primary_hostname');
+
+        if ($path = ltrim($path, '/')) {
+            $url .= '/' . $path;
+        }
+
+        if ($params) {
+            $url .= '?' . http_build_query($params);
+        }
+
+        return $url;
+    }
+}
