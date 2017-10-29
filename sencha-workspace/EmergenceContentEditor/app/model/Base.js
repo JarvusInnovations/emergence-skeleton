e67c9a23caf3aceea45613c7dc71ba19aaab180c
Chris Alfano <chris@jarv.us>
2017-10-29 18:06:38 -0400

    Remove default classes from EmergenceContentEditor app

--- sencha-workspace/EmergenceContentEditor/app/model/Base.js
+++
@@ -1,7 +0,0 @@
-Ext.define('EmergenceContentEditor.model.Base', {
-    extend: 'Ext.data.Model',
-
-    schema: {
-        namespace: 'EmergenceContentEditor.model'
-    }
-});
--- sencha-workspace/EmergenceContentEditor/app/model/Personnel.js
+++
@@ -1,7 +0,0 @@
-Ext.define('EmergenceContentEditor.model.Personnel', {
-    extend: 'EmergenceContentEditor.model.Base',
-
-    fields: [
-        'name', 'email', 'phone'
-    ]
-});
