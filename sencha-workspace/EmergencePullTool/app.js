55d21f3bf52407870a6574dca8df04da92a044b8
Chris Alfano <chris@jarv.us>
2017-10-29 16:36:42 -0400

    Remove default requires from app.js

--- sencha-workspace/EmergencePullTool/app.js
+++ sencha-workspace/EmergencePullTool/app.js
@@ -7,12 +7,6 @@ Ext.application({
 
     name: 'EmergencePullTool',
 
-    requires: [
-        // This will automatically load all classes in the EmergencePullTool namespace
-        // so that application classes do not need to require each other.
-        'EmergencePullTool.*'
-    ],
-
     // The name of the initial view to create.
     mainView: 'EmergencePullTool.view.main.Main'
 });
