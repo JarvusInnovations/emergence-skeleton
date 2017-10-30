3e8d304d231a0f6a1a5d891e3f674e2d0ee8f78e
Chris Alfano <chris@jarv.us>
2017-10-30 15:59:16 -0400

    Load emergence-cms and emergence-cms-summaries packages into EmergenceContentEditor app

--- sencha-workspace/EmergenceContentEditor/app/Application.js
+++ sencha-workspace/EmergenceContentEditor/app/Application.js
@@ -5,6 +5,13 @@
  */
 Ext.define('EmergenceContentEditor.Application', {
     extend: 'Ext.app.Application',
+    requires: [
+        'Ext.container.Viewport',
+
+        /* global Emergence */
+        'Emergence.util.API',
+        'Emergence.cms.view.DualView'
+    ],
 
     name: 'EmergenceContentEditor',
 
@@ -13,6 +20,44 @@ Ext.define('EmergenceContentEditor.Application', {
         desktop: {
             quickTips: true
         }
+    },
+
+    launch: function () {
+        var siteEnv = window.SiteEnvironment || {},
+            viewportEl = Ext.get('app-viewport'),
+            editorConfig = {},
+            mainView;
+
+        // configure editor
+        if (siteEnv.cmsComposers) {
+            editorConfig.composers = siteEnv.cmsComposers;
+        }
+
+        if (siteEnv.cmsContent) {
+            editorConfig.contentRecord = siteEnv.cmsContent;
+        }
+
+        // instantiate editor
+        mainView = Ext.create('Emergence.cms.view.DualView', {
+            editorConfig: editorConfig
+        });
+
+        // load DualView UI into viewport element or created viewport container
+        if (viewportEl) {
+            viewportEl.empty();
+            mainView.render(viewportEl);
+
+            viewportEl.on('resize', function(el, info) {
+                mainView.setWidth(info.contentWidth);
+            });
+        } else {
+            Ext.create('Ext.container.Viewport', {
+                layout: 'fit',
+                items: mainView
+            });
+        }
+
+        this.callParent();
     },
 
     onAppUpdate: function () {
