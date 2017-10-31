0f2196de5e74d02438ae7778a9d50bead51abd9a
Chris Alfano <chris@jarv.us>
2017-10-31 01:21:09 -0400

    Remove default clasess

--- sencha-workspace/EmergenceEditor/app/model/Base.js
+++
@@ -1,7 +0,0 @@
-Ext.define('EmergenceEditor.model.Base', {
-    extend: 'Ext.data.Model',
-
-    schema: {
-        namespace: 'EmergenceEditor.model'
-    }
-});
--- sencha-workspace/EmergenceEditor/app/model/Personnel.js
+++
@@ -1,7 +0,0 @@
-Ext.define('EmergenceEditor.model.Personnel', {
-    extend: 'EmergenceEditor.model.Base',
-
-    fields: [
-        'name', 'email', 'phone'
-    ]
-});
