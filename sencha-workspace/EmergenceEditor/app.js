cbf43aad96e9f132d33142e25270f80d071b2429
Chris Alfano <chris@jarv.us>
2017-10-31 01:42:16 -0400

    Remove default requires and mainView

--- sencha-workspace/EmergenceEditor/app.js
+++ sencha-workspace/EmergenceEditor/app.js
@@ -5,14 +5,5 @@
 Ext.application({
     extend: 'EmergenceEditor.Application',
 
-    name: 'EmergenceEditor',
-
-    requires: [
-        // This will automatically load all classes in the EmergenceEditor namespace
-        // so that application classes do not need to require each other.
-        'EmergenceEditor.*'
-    ],
-
-    // The name of the initial view to create.
-    mainView: 'EmergenceEditor.view.main.Main'
+    name: 'EmergenceEditor'
 });
