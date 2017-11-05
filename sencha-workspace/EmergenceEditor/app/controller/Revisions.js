6b6dbfdcaef98e61289ed9ffd96ca3ee9435d7f0
Chris Alfano <chris@jarv.us>
2017-11-05 16:17:27 -0500

    Begin cleanup of revision context menu and revisions controller

--- sencha-workspace/EmergenceEditor/app/controller/Revisions.js
+++ sencha-workspace/EmergenceEditor/app/controller/Revisions.js
@@ -1,124 +1,54 @@
-/* jslint browser: true, undef: true, white: false, laxbreak: true *//* global Ext, EmergenceEditor*/
 Ext.define('EmergenceEditor.controller.Revisions', {
     extend: 'Ext.app.Controller',
 
-    views: ['Revisions', 'contextmenu.RevisionsMenu'],
-    stores: ['Revisions'],
-    models: [],
-    refs: [{
-        ref: 'revisionsGrid',
-        autoCreate: true,
-        selector: 'emergence-revisions',
-        xtype: 'emergence-revisions'
-    }, {
-        ref: 'revisionsMenu',
-        autoCreate: true,
-        selector: 'emergence-revisionsmenu',
-        xtype: 'emergence-revisionsmenu'
-    }, {
-        ref: 'diffWindow',
-        autoCreate: true,
-        selector: 'SimpleCodeViewer',
-        xtype: 'emergence-diff-viewer'
-    }, {
-        ref: 'editorTabPanel',
-        selector: 'emergence-tabpanel'
-    }],
-    onLaunch: function() {
-        // console.info('Emergence.Editor.controller.Revisions.onLaunch()');
-    },
-    init: function() {
-        // console.info('Emergence.Editor.controller.Files.init()');
 
-        // Start listening for events on views
-        this.control({
-            'emergence-revisions': {
-                itemdblclick: this.openRevision,
-                itemcontextmenu: this.onRevisionContextMenu
-            },
-            'emergence-revisionsmenu > menuitem[action=open]': {
-                click: this.onOpenClick
-            },
-            'emergence-revisionsmenu > menuitem[action=properties]': {
-                click: this.onPropertiesClick
-            },
-            'emergence-revisionsmenu > menuitem[action=compare_latest]': {
-                click: this.onCompareLatestClick
-            },
-            'emergence-revisionsmenu > menuitem[action=compare_next]': {
-                click: this.onCompareNextClick
-            },
-            'emergence-revisionsmenu > menuitem[action=compare_previous]': {
-                click: this.onComparePreviousClick
-            },
-            editorTabPanel: {
-                tabchange: this.onEditorTabChange
-            },
-            revisionsGrid: {
-                expand: this.onRevisionsGridExpand
-            }
-        });
+    views: [
+        'menu.Revision'
+    ],
 
-        this.application.on('afterloadfile', this.onAfterLoadFile, this);
-    },
-    openRevisionByRecord: function(record) {
-        Ext.util.History.add('revision:[' + record.get('ID') + ']/'+record.get('FullPath'), true);
-    },
-    openRevision: function(view, record, itemindex, event, options) {
-        this.openRevisionByRecord(record);
-    },
-    onRevisionContextMenu: function(revisionsGrid, record, item, index, event, options) {
-        event.stopEvent();
+    stores: [
+        'Revisions'
+    ],
 
-        this.currentRecord = record;
-        this.currentIndex = index;
+    refs: {
+        editorTabPanel: 'emergence-tabpanel',
+        revisionsGrid: 'emergence-revisions',
+        revisionMenu: {
+            selector: 'emergence-revisionmenu',
+            autoCreate: true,
 
-        this.getRevisionsMenu().showAt(event.getXY());
-    },
-    onPropertiesClick: function(menuItem, event, options) {
-        var data = this.currentRecord.raw;
-
-        var html = '';
-
-        for (var key in data) {
-            html += key + ': ' + data[key] + '<br>\n';
-        }
-
-        Ext.create('Ext.window.Window', {
-            title: data.Handle,
-            height: 300,
-            width: 375,
-            layout: 'fit',
-            html: html
-        }).show();
-    },
-    onOpenClick: function(menuItem, event, options) {
-        this.openRevisionByRecord(this.currentRecord);
-    },
-    onCompareLatestClick: function(menuItem, event, options) {
-        this.openDiff(this.currentRecord, this.currentRecord.store.data.get(0));
-    },
-    onCompareNextClick: function(menuItem, event, options) {
-        this.openDiff(this.currentRecord, this.currentRecord.store.data.get(this.currentIndex-1));
-    },
-    onComparePreviousClick: function(menuItem, event, options) {
-        this.openDiff(this.currentRecord.store.data.get(this.currentIndex+1), this.currentRecord);
-    },
-    openDiff: function(sideA, sideB) {
-        Ext.util.History.add('diff:[' + sideA.get('ID') + ',' + sideB.get('ID') + ']/'+sideA.get('FullPath'), true);
-    },
-
-    onAfterLoadFile: function(editor, revisionID, response) {
-        // load revisions
-        var revisionsPanel = this.getRevisionsGrid();
-
-        if (revisionsPanel.isVisible(true)) {
-            revisionsPanel.store.load({
-                params: { ID: revisionID }
-            });
+            xtype: 'emergence-revisionmenu'
         }
     },
 
+    control: {
+        editorTabPanel: {
+            tabchange: 'onEditorTabChange'
+        },
+        revisionsGrid: {
+            expand: 'onRevisionsGridExpand',
+            itemdblclick: 'onRevisionDoubleClick',
+            itemcontextmenu: 'onRevisionContextMenu'
+        },
+        // 'emergence-revisionsmenu > menuitem[action=open]': {
+        //     click: this.onOpenClick
+        // },
+        // 'emergence-revisionsmenu > menuitem[action=properties]': {
+        //     click: this.onPropertiesClick
+        // },
+        // 'emergence-revisionsmenu > menuitem[action=compare_latest]': {
+        //     click: this.onCompareLatestClick
+        // },
+        // 'emergence-revisionsmenu > menuitem[action=compare_next]': {
+        //     click: this.onCompareNextClick
+        // },
+        // 'emergence-revisionsmenu > menuitem[action=compare_previous]': {
+        //     click: this.onComparePreviousClick
+        // },
+    },
+
+
+    // event handlers
     onEditorTabChange: function(editorTabPanel, card) {
         var revisionsGrid = this.getRevisionsGrid(),
             revisionsStore = this.getRevisionsStore(),
@@ -144,5 +74,60 @@ Ext.define('EmergenceEditor.controller.Revisions', {
         if (revisionsStore.getProxy().isExtraParamsDirty()) {
             revisionsStore.load();
         }
-    }
+    },
+
+    onRevisionDoubleClick: function(gridView, revision) {
+        this.redirectTo(revision);
+    },
+
+    onRevisionContextMenu: function(revisionsGrid, revision, itemDom, index, event) {
+        var revisionMenu = this.getRevisionMenu();
+
+        event.stopEvent();
+
+        revisionMenu.setRevision(revision);
+        revisionMenu.down('[action=revision-compare-latest]').setDisabled(index == 0);
+        revisionMenu.down('[action=revision-compare-next]').setDisabled(index == 0);
+        revisionMenu.down('[action=revision-compare-previous]').setDisabled(index + 1 == revisionsGrid.getStore().getCount());
+
+        revisionMenu.showAt(event.getXY());
+
+    },
+
+
+
+
+    // legacy
+    // onPropertiesClick: function(menuItem, event, options) {
+    //     var data = this.currentRecord.raw;
+
+    //     var html = '';
+
+    //     for (var key in data) {
+    //         html += key + ': ' + data[key] + '<br>\n';
+    //     }
+
+    //     Ext.create('Ext.window.Window', {
+    //         title: data.Handle,
+    //         height: 300,
+    //         width: 375,
+    //         layout: 'fit',
+    //         html: html
+    //     }).show();
+    // },
+    // onOpenClick: function(menuItem, event, options) {
+    //     this.openRevisionByRecord(this.currentRecord);
+    // },
+    // onCompareLatestClick: function(menuItem, event, options) {
+    //     this.openDiff(this.currentRecord, this.currentRecord.store.data.get(0));
+    // },
+    // onCompareNextClick: function(menuItem, event, options) {
+    //     this.openDiff(this.currentRecord, this.currentRecord.store.data.get(this.currentIndex-1));
+    // },
+    // onComparePreviousClick: function(menuItem, event, options) {
+    //     this.openDiff(this.currentRecord.store.data.get(this.currentIndex+1), this.currentRecord);
+    // },
+    // openDiff: function(sideA, sideB) {
+    //     Ext.util.History.add('diff:[' + sideA.get('ID') + ',' + sideB.get('ID') + ']/'+sideA.get('FullPath'), true);
+    // }
 });
---
+++ sencha-workspace/EmergenceEditor/app/view/menu/Revision.js
@@ -0,0 +1,37 @@
+Ext.define('EmergenceEditor.view.menu.Revision', {
+    extend: 'Ext.menu.Menu',
+    xtype: 'emergence-revisionmenu',
+
+
+    config: {
+        revision: null
+    },
+
+    items: [
+        {
+            text: 'Open',
+            action: 'revision-open',
+            iconCls: 'x-fa fa-file-o'
+        },
+        {
+            text: 'Properties',
+            action: 'revision-properties',
+            iconCls: 'x-fa fa-list'
+        },
+        {
+            text: 'Compare Latest',
+            action: 'revision-compare-latest',
+            iconCls: 'x-fa fa-fast-forward'
+        },
+        {
+            text: 'Compare Next',
+            action: 'revision-compare-next',
+            iconCls: 'x-fa fa-arrow-up'
+        },
+        {
+            text: 'Compare Previous',
+            action: 'revision-compare-previous',
+            iconCls: 'x-fa fa-arrow-down'
+        }
+    ]
+});
