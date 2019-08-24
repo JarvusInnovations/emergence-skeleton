a3117671962d09b123e3355951e2761aee206d7f
Chris Alfano <chris@jarv.us>
2019-08-24 03:54:37 +0000

    chore: add --fetch to temporary project script

--- project-all.sh
+++ project-all.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 git holo project emergence-skeleton \
+    --fetch \
     --ref=origin/releases/v1 \
     --commit-to=emergence/skeleton/v1
 
 git holo project emergence-vfs-site \
+    --fetch \
     --ref=origin/releases/v1 \
     --commit-to=emergence/vfs-site/v1
 
 git holo project emergence-vfs-skeleton \
+    --fetch \
     --ref=origin/releases/v1 \
     --commit-to=emergence/vfs-skeleton/v1
