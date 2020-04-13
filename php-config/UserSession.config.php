7e091086df3c22d6fb85b94206fad3012261ded2
Tyler Wiest <tyler@jarv.us>
2020-04-12 20:45:07 -0400

    refactor: remove supporting person classes in favor of layer-person

--- php-config/UserSession.config.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-# Uncomment the line below to enable JWT authentication (see php-config/Emergence/Authenticators/JWT.php for details)
-# UserSession::$defaultAuthenticator = Emergence\Authenticators\JWT::class;
