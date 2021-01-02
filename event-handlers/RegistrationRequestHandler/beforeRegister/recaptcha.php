95cdfe28698586829d8a46150cf9c3f7e0704084
Chris Alfano <chris@jarv.us>
2021-01-02 01:35:52 +0000

    refactor: use Emergence\Site\Client::getAddress() method

--- event-handlers/RegistrationRequestHandler/beforeRegister/recaptcha.php
+++ event-handlers/RegistrationRequestHandler/beforeRegister/recaptcha.php
@@ -1,7 +1,7 @@
 <?php
 
 if ($recaptcha = \RemoteSystems\ReCaptcha::getInstance()) {
-    $recaptchaResponse = $recaptcha->verify($_EVENT['requestData']['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
+    $recaptchaResponse = $recaptcha->verify($_EVENT['requestData']['g-recaptcha-response'], Emergence\Site\Client::getAddress());
 
     if (!$recaptchaResponse->isSuccess()) {
         $_EVENT['additionalErrors']['ReCaptcha'] = 'Please prove that you aren\'t a spam robot by completing the reCAPTCHA';
