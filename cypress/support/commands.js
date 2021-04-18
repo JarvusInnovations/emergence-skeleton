92d605213189c19f5b359f1e71a52042e52cf095
Chris Alfano <chris@jarv.us>
2021-04-17 20:19:10 -0400

    feat(e2e): enable loginAs custom password

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,72 +26,72 @@
 
 // from https://github.com/javieraviles/cypress-upload-file-post-form
 Cypress.Commands.add('upload_file', (fileName, fileType = ' ', selector) => {
-    cy.get(selector).then(subject => {
-      cy.fixture(fileName, 'base64')
-        .then(Cypress.Blob.base64StringToBlob)
-        .then(blob => {
-          const el = subject[0]
-          const testFile = new File([blob], fileName, { type: fileType })
-          const dataTransfer = new DataTransfer()
-          dataTransfer.items.add(testFile)
-          el.files = dataTransfer.files
-        })
-    })
-  });
+  cy.get(selector).then(subject => {
+    cy.fixture(fileName, 'base64')
+      .then(Cypress.Blob.base64StringToBlob)
+      .then(blob => {
+        const el = subject[0]
+        const testFile = new File([blob], fileName, { type: fileType })
+        const dataTransfer = new DataTransfer()
+        dataTransfer.items.add(testFile)
+        el.files = dataTransfer.files
+      })
+  })
+});
 
 // Login command
-Cypress.Commands.add('loginAs', (user) => {
-  cy.visit('/');
-  cy.request({
-      method: 'POST',
-      url: '/login/?format=json',
-      form: true,
-      body: {
-          '_LOGIN[username]': user,
-          '_LOGIN[password]': user,
-          '_LOGIN[returnMethod]': 'POST'
-      }
-  });
+Cypress.Commands.add('loginAs', (user, password) => {
+cy.visit('/');
+cy.request({
+    method: 'POST',
+    url: '/login/?format=json',
+    form: true,
+    body: {
+        '_LOGIN[username]': user,
+        '_LOGIN[password]': password || user,
+        '_LOGIN[returnMethod]': 'POST'
+    }
+});
 });
 
 // Drop and load database in one step
 Cypress.Commands.add('resetDatabase', () => {
-  cy.dropDatabase();
-  cy.loadDatabase();
+cy.dropDatabase();
+cy.loadDatabase();
 });
 
 // Drops the entire
 Cypress.Commands.add('dropDatabase', () => {
-  cy.exec(`echo 'DROP DATABASE IF EXISTS \`default\`; CREATE DATABASE \`default\`;' | ${_buildHabExec('core/mysql', 'mysql')} -u root -h 127.0.0.1`);
+cy.exec(`echo 'DROP DATABASE IF EXISTS \`default\`; CREATE DATABASE \`default\`;' | ${_buildHabExec('core/mysql', 'mysql')} -u root -h 127.0.0.1`);
 });
 
 // Reload the original data fixtures
 Cypress.Commands.add('loadDatabase', () => {
-  cy.exec(`cat cypress/fixtures/database/*.sql | ${_buildHabExec('core/mysql', 'mysql')} -u root -h 127.0.0.1 default`);
+cy.exec(`cat cypress/fixtures/database/*.sql | ${_buildHabExec('core/mysql', 'mysql')} -u root -h 127.0.0.1 default`);
 });
 
 // Ext command getter
 Cypress.Commands.add('withExt', () => {
-  cy.window().then((win) => {
-    const Ext = win.Ext;
-    return {
-      Ext: win.Ext,
-      extQuerySelector: query => Ext.ComponentQuery.query(query)[0],
-      extQuerySelectorAll: query => Ext.ComponentQuery.query(query)
-    };
-  });
+cy.window().then((win) => {
+  const Ext = win.Ext;
+  return {
+    Ext: win.Ext,
+    extQuerySelector: query => Ext.ComponentQuery.query(query)[0],
+    extQuerySelectorAll: query => Ext.ComponentQuery.query(query)
+  };
+});
 });
 
 // private method
 function _buildHabExec(pkg, pkgCmd) {
-  const studioContainer = Cypress.env('STUDIO_CONTAINER') || null;
-  const studioSSH = Cypress.env('STUDIO_SSH') || null;
+const studioContainer = Cypress.env('STUDIO_CONTAINER') || null;
+const studioSSH = Cypress.env('STUDIO_SSH') || null;
 
-  let cmd = `hab pkg exec ${pkg} ${pkgCmd}`;
+let cmd = `hab pkg exec ${pkg} ${pkgCmd}`;
 
-  if (studioContainer) {
-    cmd = `${studioSSH ? `ssh ${studioSSH}` : ''} docker exec -i ${studioContainer} ${cmd}`;
-  }
+if (studioContainer) {
+  cmd = `${studioSSH ? `ssh ${studioSSH}` : ''} docker exec -i ${studioContainer} ${cmd}`;
+}
 
-  return cmd;
+return cmd;
 }
