bfa0b4de2ad711f33549ff94f1280e25bcdf6288
Chris Alfano <chris@jarv.us>
2021-06-12 13:31:05 -0400

    test(blog): add failing edit test

--- cypress/integration/blog.js
+++ cypress/integration/blog.js
@@ -24,7 +24,7 @@ describe('Blogging', () => {
         cy.get('input[id^=datefield-]')
             .type('{selectall}01/02/2030');
 
-            cy.get('input[id^=timefield-]')
+        cy.get('input[id^=timefield-]')
             .type('{selectall}4:05 pm{enter}');
 
         cy.get('.x-component.emergence-cms-preview time[data-ref="timeEl"]')
@@ -71,5 +71,40 @@ describe('Blogging', () => {
                 .should('have.attr', 'href', '/tags/tag2')
                 .should('have.text', 'tag2');
         });
+    });
+
+    it('Edit post', () => {
+        cy.loginAs('user', 'user');
+
+        cy.visit('/drafts');
+
+        cy.contains('Hello world!')
+            .should('have.attr', 'href', '/blog/hello_world')
+            .click();
+
+        cy.contains('Edit')
+            .should('have.attr', 'href', '/blog/hello_world/edit')
+            .click();
+
+        cy.get('.x-panel.emergence-cms-editor .x-btn[id^=emergence-cms-toolbar-][id$=-menu-trigger]')
+            .click();
+
+        cy.contains('.x-menu-item-text[id^=menuitem-]', '1/2/2030 4:05 pm')
+            .click();
+
+        cy.get('input[id^=timefield-]')
+            .type('{selectall}6:07 pm{enter}');
+
+        cy.get('.x-component.emergence-cms-preview time[data-ref="timeEl"]')
+            .should('contain', 'Wednesday, January 2, 2030 at 6:07 pm')
+            .click();
+
+        cy.contains('.x-btn.save-btn', 'Save')
+            .click();
+
+        cy.reload();
+
+        cy.get('.x-component.emergence-cms-preview time[data-ref="timeEl"]')
+            .should('contain', 'Wednesday, January 2, 2030 at 6:07 pm');
     });
 });
